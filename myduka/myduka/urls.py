306efc519ad2cc1a650c04a5696d9a95e4f15ef2
Haron M Maina <adrian.maina@student.moringaschool.com>
2025-07-30 21:12:37 +0300

    Edits urls.py

--- myduka/myduka/urls.py
+++ myduka/myduka/urls.py
@@ -3,8 +3,10 @@
 # =======================================================================
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 urlpatterns = [
+    path("api/v1/ping/", lambda request: JsonResponse({"status": "ok"})),
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('users.urls')),
     path('api/v1/reports/', include('reports.urls')),
